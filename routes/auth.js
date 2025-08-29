27e067da062c9092643e7c1411a10f695b16b23c
SairajDcoder <sairajpatil3606t@gmail.com>
2025-08-30 00:36:15 +0530

    Remove node_modules from repository and add .gitignore

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,61 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+
+const auth = (req, res, next) => {
+    const raw = req.header("Authorization");
+    const token = raw?.startsWith("Bearer ") ? raw.replace("Bearer ", "") : null;
+    console.log(token);
+
+    if (!token)
+        return res.status(401).json({ error: "Access denied: missing token" });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = { id: decoded.id };
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Invalid or expired token" });
+    }
+};
+
+const validateSignup = [
+    body("name")
+        .isString().withMessage("Name must be a string")
+        .notEmpty().withMessage("Name is required")
+        .trim()
+        .isLength({ min: 3, max: 30 }).withMessage("Name must be between 3 and 30 characters"),
+
+    body("email")
+        .isEmail().withMessage("Please provide a valid email")
+        .normalizeEmail(),
+
+    body("password")
+        .isString().withMessage("Password must be a string")
+        .notEmpty().withMessage("Password is required")
+        .isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+];
+
+const validateLogin = [
+    body("name")
+        .isString().withMessage("Name must be a string")
+        .notEmpty().withMessage("Name is required"),
+
+    body("email")
+        .isString().withMessage("Email must be a string")
+        .notEmpty().withMessage("Email is required"),
+
+    body("password")
+        .isString().withMessage("Password must be a string")
+        .notEmpty().withMessage("Password is required"),
+];
 
 // Register Route
-router.post("/register", async (req, res) => {
+router.post("/register", validateSignup, async (req, res) => {
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(400).json({ errors: errors.array() });
         const { name, email, password } = req.body;
 
         let user = await User.findOne({ email });
@@ -20,12 +71,56 @@ router.post("/register", async (req, res) => {
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-        res.json({ msg: "User registered successfully" });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.status(201).json({
+            message: "User created successfully",
+            token,
+            user: {
+                id: user._id
+            },
+        });
     } catch (err) {
         console.error(err.message);   // 👈 log actual error
         res.status(500).json({ msg: "Server error", error: err.message });
     }
 });
+
+//Login Route
+router.post("/login", validateLogin, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(400).json({ errors: errors.array() });
+
+        const { name, email, password } = req.body;
+
+        const user = await User.findOne({ name });
+        if (!user) return res.status(401).json({ error: "Invalid credentials" });
+
+        const ok = await bcrypt.compare(password, user.password);
+        if (!ok) return res.status(401).json({ error: "Invalid credentials" });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.json({ token });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// for test purpose to check which users are there in db
+router.get("/users", auth, async (req, res) => {
+    try {
+        const users = await User.find().select("-password"); // exclude password
+        res.json(users);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: "Server error", error: err.message });
+    }
+});
+
+
 
 
 
--- server.js
+++ server.js
@@ -12,14 +12,13 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+    .catch(err => console.log(err));
 
 // ✅ Import routes
 const authRoutes = require("./routes/auth");
-app.use("/api/auth", authRoutes);
 
 // ✅ Use routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
